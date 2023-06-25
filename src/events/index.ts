4ee1e37f837eb1b25b491672004f9dd001b37607
Damian Gorol <damian.gorol78@gmail.com>
2023-06-25 23:02:51 +0200

    ŚRODA 🗿🗿🗿

--- src/events/index.ts
+++ src/events/index.ts
@@ -1,10 +1,12 @@
 import { Event } from '../types';
 import ready from './ready';
 import interactionCreate from './interactionCreate';
+import weekday from './weekday';
 
 const events: Event<any>[] = [
     ready,
     interactionCreate,
+    weekday
 ]
 
 export default events;
