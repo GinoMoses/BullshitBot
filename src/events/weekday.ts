fed519853218ee1a360ae46b80a64a201cadeaf1
Damian Gorol <damian.gorol78@gmail.com>
2023-06-28 18:20:46 +0200

    skill issue

--- src/events/weekday.ts
+++ src/events/weekday.ts
@@ -3,17 +3,18 @@ import { TextChannel } from 'discord.js';
 import schedule from 'node-schedule';
 
 export default event('ready', async ({ log }, client) => {
-    const weekDays = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
+    // will fix one day
+    // const weekDays = ['niedziela', 'poniedziałek', 'wtorek', 'środa', 'czwartek', 'piątek', 'sobota'];
 
-    const rule = new schedule.RecurrenceRule();
-	rule.hour = 0;
-	rule.tz = 'CET';
-	const job = schedule.scheduleJob(rule, async function() {
-        let deleted: any;
-        do {
-            deleted = (client.channels.cache.get("991707404209229865")! as TextChannel).bulkDelete(100);
-        } while (deleted.size != 0);
-        await (client.channels.cache.get("991707404209229865")! as TextChannel).setName(`${weekDays[new Date().getDay()]}`)
-        await (client.channels.cache.get("991707404209229865")! as TextChannel).send({ files: [`assets/${weekDays[new Date().getDay()]}.png`] });
-    });		
+    // const rule = new schedule.RecurrenceRule();
+	// rule.hour = 0;
+	// rule.tz = 'CET';
+	// const job = schedule.scheduleJob(rule, async function() {
+    //     let deleted: any;
+    //     do {
+    //         deleted = (client.channels.cache.get("991707404209229865")! as TextChannel).bulkDelete(100);
+    //     } while (deleted.size != 0);
+    //     await (client.channels.cache.get("991707404209229865")! as TextChannel).setName(`${weekDays[new Date().getDay()]}`);
+    //     await (client.channels.cache.get("991707404209229865")! as TextChannel).send({ files: [`assets/${weekDays[new Date().getDay()]}.png`] });
+    // });		
 })
